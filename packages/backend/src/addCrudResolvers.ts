d6de0b4a8aaaab2855794e9eb425455e851bd5b3
Luboš Matejčík <lubos.matejcik@gmail.com>
2022-08-04 11:30:21 +0200

    fix: typing of args in customize resolve functions (#116)
    BREAKING CHANGE: aliasPrefix type changed

--- packages/backend/src/addCrudResolvers.ts
+++ packages/backend/src/addCrudResolvers.ts
@@ -4,10 +4,12 @@ import { extendType, arg, intArg } from "nexus";
 import * as Helpers from "nexus-plugin-prisma/src/typegen/helpers";
 
 const addCrudResolvers = <
+  AliasPrefix extends string,
   ModelName extends keyof Helpers.GetGen<"outputs"> & string,
 >(
   resourceName: ModelName,
-  options?: ResourceOptions<ModelName> & CommonOptions,
+  options?: ResourceOptions<AliasPrefix, ModelName> &
+    CommonOptions<AliasPrefix>,
 ) => setupCrudResolvers({ extendType, arg, intArg }, resourceName, options);
 
 export default addCrudResolvers;
--- packages/dataprovider/src/testUtils/getTestIntrospection.ts
+++ packages/dataprovider/src/testUtils/getTestIntrospection.ts
@@ -7,9 +7,9 @@ import { CommonOptions } from "../../../backend/src/types";
 import { defaultOurOptions } from "../buildDataProvider";
 import { testSchemaTypeGraphql } from "../../test-data/testSchemaTypeGraphql";
 
-const buildIntrospection = async (
+const buildIntrospection = async <Prefix extends string>(
   rawSchema: GraphQLSchema,
-  options: CommonOptions,
+  options?: CommonOptions<Prefix>,
 ) => {
   const schema = await graphql(rawSchema, getIntrospectionQuery()).then(
     ({ data: { __schema } }) => __schema,
@@ -20,7 +20,9 @@ const buildIntrospection = async (
   }) as IntrospectionResult;
 };
 
-export const getTestIntrospectionNexus = async (options?: CommonOptions) => {
+export const getTestIntrospectionNexus = async <Prefix extends string>(
+  options?: CommonOptions<Prefix>,
+) => {
   return await buildIntrospection(
     testSchemaNexus({
       ...options,
@@ -30,8 +32,8 @@ export const getTestIntrospectionNexus = async (options?: CommonOptions) => {
   );
 };
 
-export const getTestIntrospectionTypeGraphql = async (
-  options?: CommonOptions,
+export const getTestIntrospectionTypeGraphql = async <Prefix extends string>(
+  options?: CommonOptions<Prefix>,
 ) => {
   return await buildIntrospection(
     await testSchemaTypeGraphql({
